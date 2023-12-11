25e93e3fba5e7020827dca47a0c30348073c0182
adam <adam@ZMIEN-MNIE>
2023-12-11 15:24:44 +0100

    Add project files.

---
+++ Zadanie/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj ścieżkę do pliku: ");
+        string filePath = Console.ReadLine();
+        if (File.Exists(filePath))
+        {
+            string content = File.ReadAllText(filePath);
+            Console.WriteLine("Zawartość pliku:");
+            Console.WriteLine(content);
+        }
+        else
+        {
+            Console.WriteLine("Plik nie istnieje. Tworzenie nowego pliku.");
+            using (StreamWriter sw = File.CreateText(filePath))
+            {
+                Console.WriteLine("Wprowadzaj tekst (wpisz 'END;¡' aby zakończyć):");
+                string line;
+                do
+                {
+                    line = Console.ReadLine();
+                    if (line != "END;/")
+                        sw.WriteLine(line);
+                } while (line != "END;/");
+            }
+            Console.WriteLine("Zapisano tekst do pliku.");
+        }
+    }
+}
